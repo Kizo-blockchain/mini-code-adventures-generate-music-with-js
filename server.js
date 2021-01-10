d2765b6607e403ae5100907f7223c745da949430
bentobase <75795415+bbasemeghan@users.noreply.github.com>
2021-01-11 00:21:56 +0100

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const createMidiForToneJs = () => {
 
@@ -92,7 +92,6 @@ var kickArray = [
 ];
 
 var randomKickPatern = Math.floor(Math.random()*kickArray.length);
-
 
 
 
