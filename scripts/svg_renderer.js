00ba0feb3b9199198fc7d6158b17996d17e7706c
bentobase <75795415+bbasemeghan@users.noreply.github.com>
2021-01-11 00:26:12 +0100

    make it better

--- scripts/svg_renderer.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import App from './app'
-export default function(data) {
-  return ReactDOMServer.renderToStaticMarkup(<App data={data} />);
-}
--- svgGenerator.js
+++
@@ -1,6 +0,0 @@
-const svgRenderer = require('./scripts/svg_renderer.js').default;
-
-exports.getSVG = function(req, res) {
-    const svg = svgRenderer(req.query);
-    res.send(svg);
-}
